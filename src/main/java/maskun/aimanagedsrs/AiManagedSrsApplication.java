f9659aedf8cde55de5890580fc385a3d135f5ded
MASKUN2 <maskunflower2@naver.com>
2025-08-18 10:48:49 +0900

    feat: Spring Boot 프로젝트 초기 설정 추가
    - Spring Boot 프로젝트 생성 및 초기화
    - build.gradle 파일 생성 및 의존성 설정
    - gradle-wrapper 파일 추가 및 설정
    - gitignore 파일 추가
    - compose.yaml 파일 추가 및 PostgreSQL 컨테이너 설정

---
+++ src/main/java/maskun/aimanagedsrs/AiManagedSrsApplication.java
@@ -0,0 +1,13 @@
+package maskun.aimanagedsrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiManagedSrsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiManagedSrsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/maskun/aimanagedsrs/AiManagedSrsApplicationTests.java
@@ -0,0 +1,13 @@
+package maskun.aimanagedsrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiManagedSrsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
