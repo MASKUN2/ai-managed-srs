02ca457922ffe788091c9f9749c8d01862e65434
MASKUN2 <maskunflower2@naver.com>
2025-09-09 17:09:33 +0900

    ChatApi에 메시지 전송 API 추가
    - @GetMapping 애너테이션으로 "/api/v1/chat" 경로 매핑
    - 메시지를 처리하는 send 메서드 구현 및 ResponseEntity 반환

--- src/main/java/maskun/aimanagedsrs/adaptors/ChatApi.java
+++ src/main/java/maskun/aimanagedsrs/adaptors/ChatApi.java
@@ -3,6 +3,7 @@ package maskun.aimanagedsrs.adaptors;
 import lombok.RequiredArgsConstructor;
 import maskun.aimanagedsrs.hexagon.chat.provided.Chat;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +12,7 @@ public class ChatApi {
     static final String CHAT_API_URL = "/api/v1/chat";
     final Chat chat;
 
+    @GetMapping(CHAT_API_URL)
     public ResponseEntity<String> send(String message) {
         return ResponseEntity.ok(chat.send(message));
     }
