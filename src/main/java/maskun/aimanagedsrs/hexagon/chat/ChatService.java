ddc99467ff863c8c49a3268ca68609cc17a4cd90
MASKUN2 <maskunflower2@naver.com>
2025-09-09 17:08:48 +0900

    ChatClientConfig 클래스 추가
    - ChatClient 빈을 생성하는 ChatClientConfig 클래스 생성
    - ChatService 생성자에 @RequiredArgsConstructor 애너테이션 추가
    - ChatClient 빌더를 빈으로 주입받도록 리팩토링

--- src/main/java/maskun/aimanagedsrs/hexagon/chat/ChatService.java
+++ src/main/java/maskun/aimanagedsrs/hexagon/chat/ChatService.java
@@ -1,17 +1,15 @@
 package maskun.aimanagedsrs.hexagon.chat;
 
+import lombok.RequiredArgsConstructor;
 import maskun.aimanagedsrs.hexagon.chat.provided.Chat;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ChatService implements Chat {
 
     private final ChatClient client;
-
-    public ChatService(ChatClient.Builder builder) {
-        this.client = builder.build();
-    }
 
     @Override
     public String send(String message) {
