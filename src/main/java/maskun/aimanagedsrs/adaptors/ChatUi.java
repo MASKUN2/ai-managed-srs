abb887b8760b8b43a65a9021ce544ccf42d5886d
MASKUN2 <maskunflower2@naver.com>
2025-09-09 17:05:26 +0900

    ChatUi 클래스 및 테스트 패키지 이동
    - ChatUi 클래스와 ChatUiTest 클래스의 패키지를 adaptors/ui에서 adaptors로 변경
    - 관련 import 및 애너테이션 수정

--- src/main/java/maskun/aimanagedsrs/adaptors/ChatUi.java
+++ src/main/java/maskun/aimanagedsrs/adaptors/ChatUi.java
@@ -1,4 +1,4 @@
-package maskun.aimanagedsrs.adaptors.ui;
+package maskun.aimanagedsrs.adaptors;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/test/java/maskun/aimanagedsrs/adaptors/ChatUiTest.java
+++ src/test/java/maskun/aimanagedsrs/adaptors/ChatUiTest.java
@@ -1,11 +1,10 @@
-package maskun.aimanagedsrs.adaptors.ui;
+package maskun.aimanagedsrs.adaptors;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.assertj.MockMvcTester;
 
-import static org.junit.jupiter.api.Assertions.*;
 @WebMvcTest(ChatUi.class)
 class ChatUiTest {
 
