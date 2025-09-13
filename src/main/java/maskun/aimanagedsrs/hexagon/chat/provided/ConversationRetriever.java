2ecfb8bbf45995792665d1b4e727d0d277954a65
MASKUN2 <maskunflower2@naver.com>
2025-09-13 21:05:47 +0900

    대화 조회 인터페이스명 변경

--- src/main/java/maskun/aimanagedsrs/hexagon/chat/provided/ConversationRetriever.java
+++ src/main/java/maskun/aimanagedsrs/hexagon/chat/provided/ConversationRetriever.java
@@ -5,6 +5,6 @@ import maskun.aimanagedsrs.hexagon.chat.domain.Conversation;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface ConversationFinder {
+public interface ConversationRetriever {
     Optional<Conversation> findById(UUID id);
 }
