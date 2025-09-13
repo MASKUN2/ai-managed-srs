21d220b84be8c3204039f22e4b332a9d45f3de6a
MASKUN2 <maskunflower2@naver.com>
2025-09-13 20:36:22 +0900

    UUID에 @NonNull 애노테이션 추가

--- src/main/java/maskun/aimanagedsrs/hexagon/chat/provided/ConversationFinder.java
+++ src/main/java/maskun/aimanagedsrs/hexagon/chat/provided/ConversationFinder.java
@@ -1,10 +1,11 @@
 package maskun.aimanagedsrs.hexagon.chat.provided;
 
+import lombok.NonNull;
 import maskun.aimanagedsrs.hexagon.chat.domain.Conversation;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface ConversationFinder {
-    Optional<Conversation> findById(UUID id);
+    Optional<Conversation> findById(@NonNull UUID id);
 }
