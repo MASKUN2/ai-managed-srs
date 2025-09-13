8f63eba8574e43623302d65bf73cdb5ae9866fbc
MASKUN2 <maskunflower2@naver.com>
2025-09-13 21:02:47 +0900

    @NonNull 제거 및 @NullMarked 적용

--- src/main/java/maskun/aimanagedsrs/hexagon/chat/provided/ConversationFinder.java
+++ src/main/java/maskun/aimanagedsrs/hexagon/chat/provided/ConversationFinder.java
@@ -1,11 +1,10 @@
 package maskun.aimanagedsrs.hexagon.chat.provided;
 
-import lombok.NonNull;
 import maskun.aimanagedsrs.hexagon.chat.domain.Conversation;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface ConversationFinder {
-    Optional<Conversation> findById(@NonNull UUID id);
+    Optional<Conversation> findById(UUID id);
 }
---
+++ src/main/java/maskun/aimanagedsrs/hexagon/chat/provided/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package maskun.aimanagedsrs.hexagon.chat.provided;
+
+import org.jspecify.annotations.NullMarked;
