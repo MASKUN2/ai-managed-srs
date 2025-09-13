f0677cf8778eb37df86273c27066fc910364612d
MASKUN2 <maskunflower2@naver.com>
2025-09-14 01:15:20 +0900

    대화 조회 메서드 URL 변경

--- src/main/java/maskun/aimanagedsrs/adaptors/ConversationUi.java
+++ src/main/java/maskun/aimanagedsrs/adaptors/ConversationUi.java
@@ -1,22 +1,15 @@
 package maskun.aimanagedsrs.adaptors;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-import java.util.UUID;
 
 @Controller
 public class ConversationUi {
     static final String CONVERSATION_URL = "/conversation";
     public static final String CHAT_VIEW_NAME = "conversation";
 
-    @GetMapping(CONVERSATION_URL + "/{conversationId}")
-    public String getConversationView(@PathVariable UUID conversationId, Model model) {
-
-        model.addAttribute("conversationId", conversationId);
-
+    @GetMapping(CONVERSATION_URL)
+    public String getConversationView() {
         return CHAT_VIEW_NAME;
     }
 }
