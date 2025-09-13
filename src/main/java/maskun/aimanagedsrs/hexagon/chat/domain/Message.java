cc15f020461c2f8a03f8ae978dc166d407bebdd8
MASKUN2 <maskunflower2@naver.com>
2025-09-13 16:34:03 +0900

    메시지 엔티티에 equals, hashCode 및 toString 추가

--- src/main/java/maskun/aimanagedsrs/hexagon/chat/domain/Message.java
+++ src/main/java/maskun/aimanagedsrs/hexagon/chat/domain/Message.java
@@ -2,14 +2,18 @@ package maskun.aimanagedsrs.hexagon.chat.domain;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import maskun.aimanagedsrs.hexagon.UUIDv7;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.time.Instant;
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
+@Getter
 @Table(name = "message",
         indexes = @Index(name = "idx_message_created_at", columnList = "created_at"))
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -47,5 +51,30 @@ public class Message {
     public enum Role {
         USER,
         ASSISTANT;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Message message = (Message) o;
+        return id != null && Objects.equals(id, message.id);
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "(" +
+                "id = " + id + ", " +
+                "role = " + role + ", " +
+                "createdAt = " + createdAt + ", " +
+                "content = " + content + ")";
     }
 }
