cb86f5f6bfa02aa78a31cc378dfa4267a327299b
MASKUN2 <maskunflower2@naver.com>
2025-09-13 16:25:48 +0900

    대화 엔티티 equals, hashCode 및 toString 추가

--- src/main/java/maskun/aimanagedsrs/hexagon/chat/ChatService.java
+++ src/main/java/maskun/aimanagedsrs/hexagon/chat/ChatService.java
@@ -1,12 +1,14 @@
 package maskun.aimanagedsrs.hexagon.chat;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import maskun.aimanagedsrs.hexagon.chat.provided.Chat;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class ChatService implements Chat {
 
@@ -14,9 +16,13 @@ public class ChatService implements Chat {
 
     @Override
     public Flux<String> send(UserChatRequest request) {
+        final StringBuffer responseBuffer = new StringBuffer();
+
         return client.prompt()
                 .user(request.message())
                 .stream()
-                .content();
+                .content()
+                .doOnNext(responseBuffer::append)
+                .doOnComplete(() -> log.info("Chat response: {}", responseBuffer.toString()));
     }
 }
