5bc472dfae60eecc066ab85af838b8f8405bc3f7
MASKUN2 <maskunflower2@naver.com>
2025-09-14 11:00:05 +0900

    인터페이스 상속 관계 수정

--- src/main/java/maskun/aimanagedsrs/hexagon/shared/EntityFinder.java
+++ src/main/java/maskun/aimanagedsrs/hexagon/shared/EntityFinder.java
@@ -2,7 +2,7 @@ package maskun.aimanagedsrs.hexagon.shared;
 
 import java.util.Optional;
 
-public interface EntityFinder<E, ID> {
+public interface EntityFinder<E, ID> extends EntityClassProvider<E> {
 
     Optional<E> find(ID id);
 }
--- src/main/java/maskun/aimanagedsrs/hexagon/shared/RequirableEntityFinder.java
+++ src/main/java/maskun/aimanagedsrs/hexagon/shared/RequirableEntityFinder.java
@@ -2,7 +2,7 @@ package maskun.aimanagedsrs.hexagon.shared;
 
 import java.util.NoSuchElementException;
 
-public interface RequirableEntityFinder<E, ID> extends EntityFinder<E, ID>, EntityClassProvider<E> {
+public interface RequirableEntityFinder<E, ID> extends EntityFinder<E, ID> {
 
     default E require(ID id) throws NoSuchElementException {
         return find(id).orElseThrow(() -> new NoSuchElementException(getEntityClass().getSimpleName() + " 찾을 수 없습니다. id: " + id));
